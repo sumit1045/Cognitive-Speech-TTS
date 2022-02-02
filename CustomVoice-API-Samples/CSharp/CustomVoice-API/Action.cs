91b1c276b3718af408d192fd4b67a0dd025e59b2
sumit1045 <sumitkoli2002@gmail.com>
2022-02-02 10:28:56 +0530

    first commit

---
+++ CustomVoice-API-Samples/CSharp/CustomVoice-API/Action.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomVoice_API
+{
+    public enum Action
+    {
+        get,
+        getbyprojectid,
+        getbyid,
+        getbysynthesisid,
+        create,
+        delete,
+        uploaddataset,
+        uploaddatasetwithlongaudio,
+        uploaddatasetwithaudioonly,
+        call,
+        getvoices,
+        copy,
+        update,
+    }
+}
---
+++ Samples-Http/README.md
@@ -0,0 +1,9 @@
+NOTICE: recommend using the SDK samples instead of REST 
+===============================
+
+Visit here for SDK Samples: 
+
+https://github.com/Azure-Samples/cognitive-services-speech-sdk
+
+The OLD directory is no longer supported. 
+
